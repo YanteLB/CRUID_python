3d44de786c01d1ec3dd2732fb4e04a6a426f30e9
Tsabedze, Bongamusa <95YANTELB@GMAIL.COM>
2026-07-22 04:30:26 +0200

    created with vidation

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 
-from fastapi import FastAPI, HTTPException
-
+from fastapi import FastAPI, HTTPException, status, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from pydantic import BaseModel
 app = FastAPI()
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    for error in exc.errors():
+        if error["type"] == "missing":
+            field = error["loc"][-1]
+            return JSONResponse(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                content={"error": f"Field '{field}' is required."},
+            )
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"detail": exc.errors(), "body": exc.body},
+    )
 
 
 
@@ -20,16 +36,31 @@ def root():
 def health():
     return {"status": "OK"}
 
-@app.get("/tasks")
+@app.get("/tasks", status_code=status.HTTP_200_OK)
 def get_tasks():
     return tasks
 
-@app.get("/tasks/{task_id}")
+@app.get("/tasks/{task_id}", status_code=status.HTTP_200_OK)
 def get_task(task_id: int):
     task = next((task for task in tasks if task["id"] == task_id), None)
     if task is None:
-        raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"error": f"Task {task_id} not found"})
     return task
+
+class Task(BaseModel):
+    title: str
+
+
+
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+def create_task(task: Task):
+    task_id = max((task["id"] for task in tasks), default=0) + 1 if tasks else 1
+    title = task.title
+    if title is None or title.strip() == "":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Title is required")
+    new_task = {"id": task_id, "title": title+f"{task_id}", "description": f"This is task {task_id}", "completed": False}
+    tasks.append(new_task)
+    return new_task
 #def main():
  #   print("Hello from cruid-python!")
 
